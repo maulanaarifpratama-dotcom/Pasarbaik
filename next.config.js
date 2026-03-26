2e3babeea9e2c9518f65a5432aa9cd09315aea46
maulanaarifpratama-dotcom <maulana.arif.pratama@gmail.com>
2026-03-26 10:15:44 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const nextConfig = {
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "blob.vercel-storage.com",
+      "supabase.co",
+      "pasarbaik.com",
+    ],
+  },
+};
+
+module.exports = nextConfig;
